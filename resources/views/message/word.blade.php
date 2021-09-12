f1fd2eca8bf5c31238edf83b73512ec7d1f02217
ToshikiKawata <jubei.animal@gmail.com>
2021-09-12 11:11:02 +0900

    ルーティングからviewを表示

---
+++ resources/views/message/word.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VCAPP</title>
+</head>
+<body>
+    <H1>URLパラメーターを表示</H1>
+    <h2>{{ $msg }}</h2>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//ROute::Httoメソッド(URI,ACTION);
+Route::get('/route/hello', function () {
+    return '<h1>Hello from Route!';
+});
+
+Route::get('/view/hello', function () {
+    return view('message.hello');
+});
+
+Route::get('view/var' , function () {
+    $variable = 'Hello from web.php';
+    return view('message.var' , ['variable' => 'variable']);
+}) ;
+
+Route::get('view/word/{msg}', function ($msg) {
+    return view('message.word', ['msg' => $msg]);
+});
+
+Route::get('view/word2/{name}/{msg}', function ($name, $msg) {
+    return view('message.word2', [
+        'name' => $name,
+        'msg' => $msg
+    ]);
+});
